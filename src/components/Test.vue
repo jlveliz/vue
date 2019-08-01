9b929d844818222b9de1419145ba595f46d8a55e
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2019-08-01 16:39:03 -0500

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <h2>Soy Un test  {{name}} </h2>
+</template>
+
+<script>
+export default {
+   name: 'Test',
+   data: () => {
+       return {
+           name : 'de otro mundo 2'
+       }
+   }
+}
+</script>
+
+<style>
+
+</style>
+
+
