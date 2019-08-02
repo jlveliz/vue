a8aacbc5e3f6ef8fabc7af0a8a3872860a162a1e
Jorge Veliz <jorgeconsalvacion@gmail.com>
2019-08-01 21:59:24 -0500

    clase 6

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Test/>
-    <h1>Hola Mundo</h1>
+    <!-- <h1>Hola Mundo</h1> -->
   </div>
 </template>
 
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,14 +1,54 @@
 <template>
-    <h2>Soy Un test  {{name}} </h2>
+    <!-- <div v-html="myHtml">
+        <h2>{{ title }} {{user.name +' ' + user.last_name}} </h2>
+    </div> -->
+    <div>
+        <div v-if="!showName">
+            <p>No puedo mostrar el contenido</p>
+        </div>
+        <div v-else>
+            {{user.name}} {{user.last_name}}
+        </div>
+        <hr>
+        <div> 
+            <h3>Todo list</h3> 
+            <!-- <ol>
+                <li v-for="(task, idx) in tasks">{{task.title}}</li>
+            </ol> -->
+            <input type="text" v-model="user.name" v-on:keyup.enter="writeAble">
+            <button v-on:click="pressMe()" type="button">Press me!</button>
+        </div>
+    </div>
+
+   
 </template>
 
 <script>
 export default {
    name: 'Test',
-   data: () => {
+   data ()  {
        return {
-           name : 'de otro mundo 2'
+           title: 'Hola User ',
+           user: {
+               name : 'Karen',
+               last_name: 'de Veliz'
+           },
+           myHtml : '<h1>esto es un Html</h1>',
+           showName: true,
+           tasks: [
+               {title: 'Go Cinemark'},
+               {title: 'new date'}
+           ]
        }
+   },
+   methods: {
+       pressMe(e) {
+           alert("Hola " + this.user.name)
+       },
+       writeAble() {
+           console.log(this.user.name)
+       }
+       
    }
 }
 </script>
