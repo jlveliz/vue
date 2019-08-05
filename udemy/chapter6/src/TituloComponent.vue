363b23d4d631f61c4bba0066fa81ab5e64943c7d
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2019-08-05 12:59:00 -0500

    lista de tareas

---
+++ udemy/chapter6/src/TituloComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{text}}</h1>    
+</template>
+
+<script>
+
+export default {
+    name:'Titulo',
+    props: [
+        'text'
+    ]
+    
+}
+</script>
+
+<style>
+
+</style>
+
+
