df098326fca54158ef3f53ce6e3d8276a9490cd3
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2019-08-07 13:13:50 -0500

    slots

---
+++ udemy/chapter7/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="jumbotron">
+          <MostrarFormulario>
+            <h3>Recuperar Contraseña</h3>
+          </MostrarFormulario>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import MostrarFormulario from './components/MostrarFormulario'
+
+export default {
+  name: 'App',
+  components: {
+    MostrarFormulario
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ udemy/chapter7/src/components/MostrarFormulario.vue
@@ -0,0 +1,22 @@
+<template>
+    <form action="" class="form">
+        <slot></slot>
+        <div class="form-group">
+            <label for="correo">Correo Electrónico</label>
+            <input type="email" class="form-control" id="correo" placeholder="Ingrese su correo Electrónico">
+        </div>
+        <div class="form-group">
+            <label for="password">Contraseña</label>
+            <input type="password" id="password" class="form-control" placeholder="Ingresa tu contraseña">
+        </div>
+        <button class="btn btn-primary">Iniciar Sesión</button>
+    </form>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
