393b7a1406a76876c5a11a1667c0ec3c7f10086a
Jorge Veliz <jorgeconsalvacion@gmail.com>
2019-08-08 21:41:34 -0500

    multiple slots

--- udemy/chapter7/src/App.vue
+++ udemy/chapter7/src/App.vue
@@ -3,7 +3,24 @@
     <div class="container">
       <div class="jumbotron">
           <MostrarFormulario>
-            <h3>Recuperar Contraseña</h3>
+            <div slot="titulo">
+              <h3>Recuperación de contraseña</h3>  
+            </div>
+
+            <div slot="boton">
+              <button class="btn btn-primary">Iniciar Sesión</button>
+            </div>
+
+            <div slot="elementos">
+               <div class="form-group">
+                <label for="correo">Correo Electrónico</label>
+                <input type="email" class="form-control" id="correo" placeholder="Ingrese su correo Electrónico">
+              </div>
+              <div class="form-group">
+                  <label for="password">Contraseña</label>
+                  <input type="password" id="password" class="form-control" placeholder="Ingresa tu contraseña">
+              </div>
+            </div>
           </MostrarFormulario>
       </div>
     </div>
--- udemy/chapter7/src/components/MostrarFormulario.vue
+++ udemy/chapter7/src/components/MostrarFormulario.vue
@@ -1,15 +1,9 @@
 <template>
     <form action="" class="form">
-        <slot></slot>
-        <div class="form-group">
-            <label for="correo">Correo Electrónico</label>
-            <input type="email" class="form-control" id="correo" placeholder="Ingrese su correo Electrónico">
-        </div>
-        <div class="form-group">
-            <label for="password">Contraseña</label>
-            <input type="password" id="password" class="form-control" placeholder="Ingresa tu contraseña">
-        </div>
-        <button class="btn btn-primary">Iniciar Sesión</button>
+        <slot name="titulo"></slot>
+        <slot name="boton"></slot>
+        <slot name="elementos"></slot>
+        
     </form>
 </template>
 
