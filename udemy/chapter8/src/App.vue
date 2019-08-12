2c2a19f369e81b52fd0682d2401c2e8fee9bcdc3
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2019-08-12 09:10:29 -0500

    directivas

---
+++ udemy/chapter8/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+      <div class="container">
+        <div class="jumbotron">
+          <span>Aprende Vue.js 2 Fácilmente</span>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
