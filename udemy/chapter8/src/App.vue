08c435653ab1eabe51c1df4ae465a9914d0255a4
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2019-08-12 09:39:27 -0500

    directiva local

--- udemy/chapter8/src/App.vue
+++ udemy/chapter8/src/App.vue
@@ -3,6 +3,8 @@
       <div class="container">
         <div class="jumbotron">
           <span v-decorar:grande.negrilla.italico="{familia:'helvetica',color:'red'}">Aprende Vue.js 2 Fácilmente</span>
+          <hr>
+          <span v-decorar-local="{familia:'serif',color:'green'}">Aprende Angular Fácilmente</span>
         </div>
     </div>
   </div>
@@ -18,8 +20,27 @@ export default {
       family: 'helvetica'
     }
   },
-  components: {
-    
+  directives: {
+    'decorar-local' : {
+      bind(el, binding, vnode){
+        el.style.fontFamily = binding.value.familia;
+        el.style.color = binding.value.color;
+
+        //argumentos o parametros
+        if(binding.arg == 'grande') {
+          el.style.fontSize = "80px";
+        }
+
+        //modificadores
+        if(binding.modifiers['negrilla']){
+          el.style.fontWeight = 'bold';
+        }
+      
+        if(binding.modifiers['italico']){
+          el.style.fontStyle = 'italic';
+        }
+      }
+    }
   }
 }
 </script>
