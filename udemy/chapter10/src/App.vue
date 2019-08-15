acb24e5581e7e22da1fa1b7c7b4f80fdf63be11a
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2019-08-15 08:45:42 -0500

    filtros - filtro local

---
+++ udemy/chapter10/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      {{mensaje | mayusculas}}
+      <hr/>  
+      {{mensaje }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      mensaje: 'Aprende JS 2 Fácilmente'
+    }
+  },
+  filters: {
+    mayusculas(str) {
+      return str.toUpperCase();
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
