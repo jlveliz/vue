133a4a45c91a4552a02db8819ba2322bab66ac25
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2019-08-19 12:38:25 -0500

    enrouter intro

---
+++ udemy/chapter12/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Encabezado/>
+    <div class="jumbotron"></div>
+  </div>
+</template>
+
+<script>
+
+import Encabezado from './components/Encabezado';
+
+export default {
+  name: 'App',
+  components: {
+    Encabezado
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ udemy/chapter12/src/components/Encabezado.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header clearfix">
+        <nav>
+            <ul class="nav nav-pills pull-right">
+                <li> <a href="">Inicio</a></li>
+                <li><a href="">Usuarios</a></li>  
+            </ul>
+        </nav>
+        <h3 class="text text-muted">Aplicación Usuarios</h3>    
+    </div>    
+</template>
+<script>
+export default {
+    name: 'Encabezado'
+}
+</script>
