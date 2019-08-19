feee7a9bc7e6b396304cc88be4a4895244bcec0d
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2019-08-19 13:08:00 -0500

    inserta directiva routerlink

--- udemy/chapter12/src/components/Encabezado.vue
+++ udemy/chapter12/src/components/Encabezado.vue
@@ -2,8 +2,8 @@
     <div class="header clearfix">
         <nav>
             <ul class="nav nav-pills pull-right">
-                <li> <a href="">Inicio</a></li>
-                <li><a href="">Usuarios</a></li>  
+                <li> <router-link to="/">Inicio</router-link> </li>
+                <li> <router-link to="/users">Usuarios</router-link></li>  
             </ul>
         </nav>
         <h3 class="text text-muted">Aplicación Usuarios</h3>    
